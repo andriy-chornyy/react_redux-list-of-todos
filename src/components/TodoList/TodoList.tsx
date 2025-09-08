6c24fcb073cc5bf42eb6d59aee5c20c4fb15e7df
Andrii Chornyi <ixxxx@i.ua>
2025-09-08 12:06:28 +0300

    Mix Solution

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -10,7 +10,9 @@ export const TodoList: React.FC = () => {
 
   const todos = useSelector((state: RootState) => state.todos.todos);
   const filter = useSelector((state: RootState) => state.filter);
-  const selectedTodo = useSelector((state: RootState) => state.todos.selectedTodo);
+  const selectedTodo = useSelector(
+    (state: RootState) => state.todos.selectedTodo,
+  );
 
   const normalizFilterValue = filter.query.trim().toLowerCase();
   let filteredTodos = todos;
@@ -29,82 +31,92 @@ export const TodoList: React.FC = () => {
 
   const handleClick = (todo: Todo) => {
     dispatch(setSelectedTodo(todo));
-  }
-
-  // console.log('selectedTodo?.id', selectedTodo)
-  console.log('filteredTodos:', filteredTodos);
-  console.log('length:', filteredTodos.length);
+  };
 
   return (
     <>
-      {filteredTodos.length === 0 ? (<p className="notification is-warning">
-        There are no todos matching current filter criteria
-      </p>) : (
-      <table className="table is-narrow is-fullwidth">
-        <thead>
-          <tr>
-            <th>#</th>
+      {filteredTodos.length === 0 ? (
+        <p className="notification is-warning">
+          There are no todos matching current filter criteria
+        </p>
+      ) : (
+        <table className="table is-narrow is-fullwidth">
+          <thead>
+            <tr>
+              <th>#</th>
 
-            <th>
-              <span className="icon">
-                <i className="fas fa-check" />
-              </span>
-            </th>
+              <th>
+                <span className="icon">
+                  <i className="fas fa-check" />
+                </span>
+              </th>
 
-            <th>Title</th>
-            <th> </th>
-          </tr>
-        </thead>
+              <th>Title</th>
+              <th> </th>
+            </tr>
+          </thead>
 
-        <tbody>
-          {filteredTodos.length > 0
-            ? filteredTodos.map((todo) => {
-                return (
-                  <tr
-                    data-cy="todo"
-                    key={todo.id}
-                    className={selectedTodo?.id === todo.id ? 'has-background-info-light' : ''}
-                  >
-                    <td className="is-vcentered">{todo.id}</td>
-                    <td className="is-vcentered"> </td>
+          <tbody>
+            {filteredTodos.length > 0
+              ? filteredTodos.map(todo => {
+                  return (
+                    <tr
+                      data-cy="todo"
+                      key={todo.id}
+                      className={
+                        selectedTodo?.id === todo.id
+                          ? 'has-background-info-light'
+                          : ''
+                      }
+                    >
+                      <td className="is-vcentered">{todo.id}</td>
+                      {/* <td className="is-vcentered"> </td> */}
 
-                    <td className="is-vcentered is-expanded">
-                      <p
-                        className={
-                          todo.completed ? 'has-text-success' : 'has-text-danger'
-                        }
-                      >
-                        {todo.title}
-                      </p>
-                    </td>
+                      <td className="is-vcentered">
+                        {todo.completed && (
+                          <span className="icon" data-cy="iconCompleted">
+                            <i className="fas fa-check" />
+                          </span>
+                        )}
+                      </td>
 
-                    <td className="has-text-right is-vcentered">
-                      <button
-                        data-cy="selectButton"
-                        className="button"
-                        type="button"
-                        onClick={() => handleClick(todo)}
-                      >
-                        <span className="icon">
-                          <i
-                            className={
-                              selectedTodo && selectedTodo.id === todo.id
-                                ? 'far fa-eye-slash'
-                                : 'far fa-eye'
-                            }
-                          />
-                        </span>
-                      </button>
-                    </td>
-                  </tr>
-                );
-              })
-            : null}
-        </tbody>
-      </table>
+                      <td className="is-vcentered is-expanded">
+                        <p
+                          className={
+                            todo.completed
+                              ? 'has-text-success'
+                              : 'has-text-danger'
+                          }
+                        >
+                          {todo.title}
+                        </p>
+                      </td>
 
+                      <td className="has-text-right is-vcentered">
+                        <button
+                          data-cy="selectButton"
+                          className="button"
+                          type="button"
+                          onClick={() => handleClick(todo)}
+                        >
+                          <span className="icon">
+                            <i
+                              className={
+                                selectedTodo && selectedTodo.id === todo.id
+                                  ? 'far fa-eye-slash'
+                                  : 'far fa-eye'
+                              }
+                            />
+                          </span>
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })
+              : null}
+          </tbody>
+        </table>
       )}
-
     </>
   );
 };
--- src/components/TodoModal/TodoModal.tsx
+++ src/components/TodoModal/TodoModal.tsx
@@ -75,3 +75,4 @@ export const TodoModal: React.FC = () => {
     </div>
   );
 };
+// iconCompleted
